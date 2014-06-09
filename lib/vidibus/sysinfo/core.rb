b86311885d3e75bce965d3bee597c5dca7f3eef5
punkrats <andre@webwarelab.com>
2014-06-09 15:24:53 +0200

    Let core module return a result class

--- lib/vidibus/sysinfo/core.rb
+++ lib/vidibus/sysinfo/core.rb
@@ -8,14 +8,33 @@ module Vidibus
     module Core
       extend Base
 
+      class Result < Vidibus::Sysinfo::Result
+        attrs :cpus, :cores, :sockets
+
+        def to_i
+          cpus
+        end
+
+        def to_f
+          cpus.to_f
+        end
+      end
+
       class << self
         def command()
           'lscpu'
         end
 
         def parse(output)
-          if cpus = output[/CPU\(s\):\s+(\d+)/i, 1]
-            cpus.to_i
+          sockets = output[/Socket\(s\):\s+(\d+)/i, 1]
+          cores = output[/Core\(s\) per socket:\s+(\d+)/i, 1]
+          cpus = output[/CPU\(s\):\s+(\d+)/i, 1]
+          if sockets && cores && cpus
+            Result.new({
+              sockets: sockets.to_i,
+              cores: cores.to_i,
+              cpus: cpus.to_i
+            })
           end
         end
 
--- spec/vidibus/sysinfo/core_spec.rb
+++ spec/vidibus/sysinfo/core_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-OUTPUT = 'Architecture:          x86_64
+CORE_OUTPUT = 'Architecture:          x86_64
 CPU op-mode(s):        32-bit, 64-bit
 Byte Order:            Little Endian
 CPU(s):                8
@@ -24,6 +24,13 @@ NUMA node0 CPU(s):     0-7'
 
 describe "Vidibus::Sysinfo::Core" do
   let(:this) {Vidibus::Sysinfo::Core}
+  let(:values) do
+    {
+      cpus: 8,
+      cores: 4,
+      sockets: 1
+    }
+  end
 
   describe ".command" do
     it "should return 'lscpu'" do
@@ -32,8 +39,13 @@ describe "Vidibus::Sysinfo::Core" do
   end
 
   describe ".parse" do
-    it "should return a number from valid output" do
-      this.parse(OUTPUT).should eql(8)
+    it 'should return a result instance' do
+      this.parse(CORE_OUTPUT).should be_a(Vidibus::Sysinfo::Core::Result)
+    end
+
+    it 'should initialize result with correct values' do
+      mock(Vidibus::Sysinfo::Core::Result).new(values) { true }
+      this.parse(CORE_OUTPUT)
     end
 
     it "should return nil from invalid output" do
@@ -42,14 +54,45 @@ describe "Vidibus::Sysinfo::Core" do
   end
 
   describe ".call" do
-    it "should return the number of CPU cores" do
-      stub(this).perform(this.command) {[OUTPUT, ""]}
-      this.call.should eql(8)
+    it 'should call #parse' do
+      stub(this).perform(this.command) {[CORE_OUTPUT, '']}
+      mock(this).parse(CORE_OUTPUT) { true }
+      this.call
     end
 
-    it "should yield an error if lscpu is not available" do
+    it 'should yield an error if command is not available' do
       stub(this).perform(this.command) {["       0\n", "cat: command not found: lscpu\n"]}
       expect {this.call}.to raise_error(Vidibus::Sysinfo::CallError)
+    end
+  end
+
+  describe 'Result' do
+    let(:result) do
+      Vidibus::Sysinfo::Core::Result.new(values)
+    end
+
+    it 'should respond to #cpus' do
+      result.cpus.should eq(8)
+    end
+
+    it 'should respond to #cores' do
+      result.cores.should eq(4)
+    end
+
+    it 'should respond to #sockets' do
+      result.sockets.should eq(1)
+    end
+
+    it 'should respond to #to_i' do
+      result.to_i.should eq(8)
+    end
+
+    it 'should respond to #to_f' do
+      result.to_f.should eq(8.0)
+    end
+
+    it 'should respond to #to_h' do
+      result.to_h.should eq(values)
     end
   end
 end
