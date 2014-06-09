2f52f81633cb9f5096ea8337e24c0b6476718c61
punkrats <andre@webwarelab.com>
2014-06-09 14:57:10 +0200

    Let storage module return a result class

--- lib/vidibus/sysinfo/storage.rb
+++ lib/vidibus/sysinfo/storage.rb
@@ -8,6 +8,18 @@ module Vidibus
     module Storage
       extend Base
 
+      class Result < Vidibus::Sysinfo::Result
+        attrs :total, :used, :free
+
+        def to_i
+          round(used, 0).to_i
+        end
+
+        def to_f
+          used.to_f
+        end
+      end
+
       class << self
         def command
           'df -m'
@@ -16,9 +28,15 @@ module Vidibus
         def parse(output)
           device = /(?:[\/a-z0-9]+)/
           size = /\s+(\d+)\i?/
-          if output.match(/#{device}#{size}#{size}/m)
-            amount = $2.to_f
-            gigabytes(amount, 'M')
+          if matches = output.match(/#{device}#{size}#{size}#{size}/m)
+            total = $1.to_f
+            used = $2.to_f
+            free = $3.to_f
+            Result.new({
+              total: gigabytes(total, 'M'),
+              used: gigabytes(used, 'M'),
+              free: gigabytes(free, 'M')
+            })
           end
         end
       end
--- spec/vidibus/sysinfo/storage_spec.rb
+++ spec/vidibus/sysinfo/storage_spec.rb
@@ -10,6 +10,13 @@ udev                     16068         1     16067   1% /dev
 tmpfs                    16073         0     16073   0% /dev/shm
 /dev/md1                   496        31       440   7% /boot'
   end
+  let(:values) do
+    {
+      total: 2756.13,
+      used: 1511.11,
+      free: 1106.12
+    }
+  end
 
   describe ".command" do
     it "should return 'df -m'" do
@@ -18,8 +25,13 @@ tmpfs                    16073         0     16073   0% /dev/shm
   end
 
   describe ".parse" do
-    it "should return a number from valid output" do
-      this.parse(output).should eql(1511.11)
+    it 'should return a result instance' do
+      this.parse(output).should be_a(Vidibus::Sysinfo::Storage::Result)
+    end
+
+    it 'should initialize result with correct values' do
+      mock(Vidibus::Sysinfo::Storage::Result).new(values) { true }
+      this.parse(output)
     end
 
     it "should return nil from invalid output" do
@@ -28,9 +40,40 @@ tmpfs                    16073         0     16073   0% /dev/shm
   end
 
   describe ".call" do
-    it "should return the used storage in gigabytes" do
-      stub(this).perform(this.command) {[output, ""]}
-      this.call.should eql(1511.11)
+    it 'should call #parse' do
+      stub(this).perform(this.command) {[output, '']}
+      mock(this).parse(output) { true }
+      this.call
+    end
+  end
+
+  describe 'Result' do
+    let(:result) do
+      Vidibus::Sysinfo::Storage::Result.new(values)
+    end
+
+    it 'should respond to #total' do
+      result.total.should eq(2756.13)
+    end
+
+    it 'should respond to #used' do
+      result.used.should eq(1511.11)
+    end
+
+    it 'should respond to #free' do
+      result.free.should eq(1106.12)
+    end
+
+    it 'should respond to #to_i' do
+      result.to_i.should eq(1511)
+    end
+
+    it 'should respond to #to_f' do
+      result.to_f.should eq(1511.11)
+    end
+
+    it 'should respond to #to_h' do
+      result.to_h.should eq(values)
     end
   end
 end
